dc799fb04ab951416bca0b6eb2ccbd06221d6e51
cragtok <lidfun@protonmail.com>
2023-01-02 04:15:17 +0600

    Exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,15 @@
 import { useState } from "react";
 
 const Statistics = ({ statistics }) => {
+    if (statistics.good + statistics.bad + statistics.neutral === 0) {
+        return (
+            <div>
+                <h1>statistics</h1>
+                <p>No feedback given</p>
+            </div>
+        );
+    }
+
     return (
         <div>
             <h1>statistics</h1>
