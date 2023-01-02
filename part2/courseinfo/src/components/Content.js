152c775c1c978abdefb9ff8ad600b2df343857b3
cragtok <lidfun@protonmail.com>
2023-01-03 04:07:33 +0600

    Exercise 2.2

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,7 +1,19 @@
 import Part from "./Part";
-const Content = ({ content }) =>
-    content.map((course) => (
-        <Part key={course.id} name={course.name} exercises={course.exercises} />
-    ));
-
+const Content = ({ content }) => {
+    const totalExercises = content.reduce((acc, obj) => acc + obj.exercises, 0);
+    return (
+        <>
+            {content.map((course) => (
+                <Part
+                    key={course.id}
+                    name={course.name}
+                    exercises={course.exercises}
+                />
+            ))}
+            <p style={{ fontWeight: "bold" }}>
+                Total of {totalExercises} courses
+            </p>
+        </>
+    );
+};
 export default Content;
