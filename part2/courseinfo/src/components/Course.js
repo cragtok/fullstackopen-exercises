021f9fbc2d26b239757bf041bfa18e2b3e442efe
cragtok <lidfun@protonmail.com>
2023-01-03 04:20:08 +0600

    Exercise 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,5 +1,30 @@
-import Content from "./Content";
-import Header from "./Header";
+const Header = ({ text }) => <h2>{text}</h2>;
+
+const Part = ({ name, exercises }) => {
+    return (
+        <p>
+            {name} {exercises}
+        </p>
+    );
+};
+
+const Content = ({ content }) => {
+    return (
+        <>
+            {content.map((course) => (
+                <Part
+                    key={course.id}
+                    name={course.name}
+                    exercises={course.exercises}
+                />
+            ))}
+            <p style={{ fontWeight: "bold" }}>
+                Total of {content.reduce((acc, obj) => acc + obj.exercises, 0)}{" "}
+                courses
+            </p>
+        </>
+    );
+};
 
 const Course = ({ course }) => {
     return (
