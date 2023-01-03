0d197049e99ac1ccf334db8ac233eb554e75d0be
cragtok <lidfun@protonmail.com>
2023-01-04 04:24:31 +0600

    Exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -16,6 +16,19 @@ function App() {
             .then((response) => setCountries(response.data));
     }, []);
 
+    const onShowCountry = (country) => {
+        axios
+            .get(
+                `http://api.weatherstack.com/current?access_key=${
+                    process.env.REACT_APP_API_KEY
+                }&query=${country.capital[0].split(" ").join("_")}`
+            )
+            .then((response) =>
+                setDisplayedCountry({ ...country, weather: response.data })
+            )
+            .catch((err) => console.error(err));
+    };
+
     return (
         <div className="App">
             find countries{" "}
@@ -24,8 +37,8 @@ function App() {
                 filteredCountries.length > 1 &&
                 filteredCountries.map((country) => (
                     <div key={country.cca2}>
-                        {country.name.common+" "}
-                        <button onClick={() => setDisplayedCountry(country)}>
+                        {country.name.common + " "}
+                        <button onClick={() => onShowCountry(country)}>
                             show
                         </button>
                     </div>
@@ -34,19 +47,40 @@ function App() {
                 <p>"Too many matches, specify another filter."</p>
             )}
             {displayedCountry && (
-                <div>
-                    <h2>{displayedCountry.name.common}</h2>
-                    <p>capital {displayedCountry.capital[0]}</p>
-                    <p>area {displayedCountry.area}</p>
-                    <h3>languages </h3>
-                    {Object.keys(displayedCountry.languages).map((key) => (
-                        <li key={key}>{displayedCountry.languages[key]}</li>
-                    ))}
-                    <img
-                        src={displayedCountry.flags.png}
-                        alt={`${displayedCountry.name.common} flag`}
-                    />
-                </div>
+                <>
+                    <div>
+                        <h2>{displayedCountry.name.common}</h2>
+                        <p>capital {displayedCountry.capital[0]}</p>
+                        <p>area {displayedCountry.area}</p>
+                        <h3>languages </h3>
+                        {Object.keys(displayedCountry.languages).map((key) => (
+                            <li key={key}>{displayedCountry.languages[key]}</li>
+                        ))}
+                        <img
+                            src={displayedCountry.flags.png}
+                            alt={`${displayedCountry.name.common} flag`}
+                        />
+                    </div>
+                    <div>
+                        <h3>Weather in {displayedCountry.capital}</h3>
+                        <p>
+                            temperature{" "}
+                            {displayedCountry.weather.current.temperature}{" "}
+                            Celcius
+                        </p>
+                        <img
+                            src={
+                                displayedCountry.weather.current
+                                    .weather_icons[0]
+                            }
+                            alt="weather icon"
+                        />
+                        <p>
+                            wind {displayedCountry.weather.current.wind_speed}{" "}
+                            m/s {displayedCountry.weather.current.wind_dir}
+                        </p>
+                    </div>
+                </>
             )}
         </div>
     );
