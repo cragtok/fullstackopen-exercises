828a1fc3578f493b9b2793d2ff790774fbbc780f
cragtok <lidfun@protonmail.com>
2023-01-07 04:31:51 +0600

    3.13

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+
+const Person = require("./models/person");
 
 morgan.token("body", function getId(req) {
     if (req.method === "POST") {
@@ -10,42 +13,20 @@ morgan.token("body", function getId(req) {
 const app = express();
 
 app.use(express.json());
-// app.use(express.static("build"));
+app.use(express.static("build"));
 app.use(cors());
 app.use(morgan(":method :url :status :response-time :body"));
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
-
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+    Person.find({}).then((persons) => {
+        response.json(persons);
+    });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find((p) => p.id === id);
-
-    !person ? response.status(404).end() : response.json(person);
+    Person.findById(request.params.id).then((person) => {
+        response.json(person);
+    });
 });
 
 app.get("/info", (request, response) => {
@@ -89,7 +70,7 @@ app.delete("/api/persons/:id", (request, response) => {
     response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
