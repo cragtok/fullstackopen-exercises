deec9680d69cc9494895c521433e530d4e2f0a63
cragtok <lidfun@protonmail.com>
2023-01-09 03:38:27 +0600

    Exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,9 @@
 const dummy = (blogs) => {
+    blogs;
     return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+    return blogs.reduce((acc, curr) => acc + curr.likes, 0);
+};
+module.exports = { dummy, totalLikes };
