5537e38930c6c0b5a8d3d514034e5ee7c831ab9f
cragtok <lidfun@protonmail.com>
2023-01-10 04:15:30 +0600

    Exercise 4.15

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.get("/", async (request, response) => {
+    const users = await User.find({});
+    response.json(users);
+});
+
+usersRouter.get("/:id", async (request, response, next) => {
+    try {
+        const user = await User.findById(request.params.id);
+
+        if (user) {
+            response.json(user);
+        } else {
+            response.status(404).end();
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+usersRouter.post("/", async (request, response, next) => {
+    const { username, password, name } = request.body;
+
+    const saltRounds = 10;
+    try {
+        const passwordHash = await bcrypt.hash(password, saltRounds);
+
+        const user = new User({ passwordHash, username, name });
+
+        const savedUser = await user.save();
+        response.status(201).json(savedUser);
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = usersRouter;
---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: [true, "Username is required"],
+    },
+    passwordHash: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    name: {
+        type: String,
+        required: [true, "Name is required."],
+    },
+});
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+        delete returnedObject.passwordHash;
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
