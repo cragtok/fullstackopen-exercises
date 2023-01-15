eaeda85cc2755bc1bc1ede5613bcbd8d00efd43f
cragtok <lidfun@protonmail.com>
2023-01-15 12:01:37 +0600

    Exercise 6.15

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,16 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-/*
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-    return {
-        content: anecdote,
-        id: getId(),
-        votes: 0,
-    };
-};
-*/
+import anecdoteService from "../services/anecdotes";
 
 const anecdoteSlice = createSlice({
     name: "anecdotes",
@@ -35,6 +24,13 @@ const anecdoteSlice = createSlice({
         },
     },
 });
+
+export const initializeAnecdotes = () => {
+    return async (dispatch) => {
+        const anecdotes = await anecdoteService.getAll();
+        dispatch(setAnecdotes(anecdotes));
+    };
+}
 
 export const { createAnecdote, voteAnecdote, setAnecdotes } =
     anecdoteSlice.actions;
