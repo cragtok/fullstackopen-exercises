ad865e0745b797e64f14b75f09cbdda71b4d5b9b
cragtok <lidfun@protonmail.com>
2023-01-16 10:51:53 +0600

    Exercise 7.4

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const useField = (type, name) => {
+    const [value, setValue] = useState("");
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
+    return { type, name, value, onChange };
+};
