96abf305f0297acbdfbc576ce2e96472cd198345
cragtok <lidfun@protonmail.com>
2023-01-25 13:16:08 +0600

    Exercise 8.9

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,32 +1,51 @@
+import { gql, useQuery } from "@apollo/client";
+
 const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+    const ALL_BOOKS = gql`
+        query {
+            allBooks {
+                id
+                title
+                published
+                author
+            }
+        }
+    `;
 
-  const books = []
+    const result = useQuery(ALL_BOOKS);
 
-  return (
-    <div>
-      <h2>books</h2>
+    if (!props.show) {
+        return null;
+    }
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
-}
+    if (result.loading) {
+        return <div>Loading...</div>;
+    }
 
-export default Books
+    const books = [...result.data.allBooks];
+
+    return (
+        <div>
+            <h2>books</h2>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <th></th>
+                        <th>author</th>
+                        <th>published</th>
+                    </tr>
+                    {books.map((a) => (
+                        <tr key={a.id}>
+                            <td>{a.title}</td>
+                            <td>{a.author}</td>
+                            <td>{a.published}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Books;
